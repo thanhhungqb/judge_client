4f163b898275ce964ed5ad6136b195c80f888a94
vthung <thanhhungqb@gmail.com>
2017-05-08 12:27:54 +0700

    version 2.0.0

--- lectureclient.py
+++ lectureclient.py
@@ -1,19 +1,19 @@
 #!/bin/python3
 
-# this is lecture utils to process some actions needed by lecturer
-import pprint
-import sys
+import argparse
 import base64
 import json
-import requests
-import argparse
+import pprint
 import time
+
+import requests
+import sys
 
 config = None
 host = 'http://www.cse.hcmut.edu.vn'
 
 
-def main(args):
+def main():
     """ this function is called from command line which call web service to make, configure, update tasks on online system
     
     Please function for more details about parameters
@@ -30,106 +30,129 @@ def main(args):
       note: lecturer must be check before allow students submit by add group 'test', all lecturer and TA share this group
       to test assignment files, groupName not include SPACE, suggest format HK-Sub-name e.g. 162-KTLT-a3
     """
-    global config
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--configfile', dest="configure_file", default="lecturer_client.json")
+    subparsers = parser.add_subparsers(dest="subcommand")  # this line changed
 
-    if config is None:
-        print('please check lecturer_client.json')
-        sys.exit(0)
+    subparsers.add_parser('getTasks', help="View all task with id in system (right problem)")
+    # subparsers.add_parser('getTask', help="get Task zip file").add_argument('name')
 
-    parser = argparse.ArgumentParser(description='NA')
-    parser.add_argument('-a', '--action', default='getTasks',
-                        help='action to be run: getTasks/getTask/updateTask/addPermit/reportByTaskName')
-    parser.add_argument('-p', '--par', metavar='param', type=str, nargs='+',
-                        help='parameters by order, each action have some require parameter')
+    foo_parser = subparsers.add_parser('updateTask', help="Add or update task")
+    foo_parser.add_argument('--taskconfig', dest="Task config", default="taskconfig.ini")
+    foo_parser.add_argument('--zipgrade', dest="Zip file of grade", default="grade.zip")
+
+    report_task = subparsers.add_parser('reportByTaskName', help="Get report by task name")
+    report_task.add_argument('name')
+
+    # group manager
+    subparsers.add_parser('getGroups', help="show all user groups")
+    subparsers.add_parser('cleanGroup', help="clean a group by name").add_argument('name')
+
+    subparsers.add_parser('getUsersByGroup', help="Get all user in a group by name").add_argument('name')
+
+    aug = subparsers.add_parser('addUsersGroup', help="file with many line which format: user,group")
+    aug.add_argument('file')
+
+    # permit: allow to submit open, close time
+    subparsers.add_parser('getPermits',
+                          help="Show all permit to submit in system including: group name, task id, open and close time")
+
+    permit_ = subparsers.add_parser('addPermit',
+                                    help="include group name, task name and open, close time, please see format of time")
+    permit_.add_argument('gname')
+    permit_.add_argument('tname')
+    permit_.add_argument('open')
+    permit_.add_argument('close')
+
+    subparsers.add_parser('getResultToken', help="For long time task").add_argument('token')
 
     args = parser.parse_args()
 
-    action = args.action  # args[0]
-    args = args.par  # args[1:]
+    print('subcommand', args.subcommand, args)
+
+    try:
+        with open(args.configure_file, 'r') as f:
+            config = json.loads(f.read())
+    except Exception as e:
+        print('err', str(e))
 
     with NetworkUtils(config) as nut:
+        try:
+            ret = json.loads(nut.sendData({'username': config['username'], 'password': config['password']},
+                                          url=host + '/onlinejudge/ws/auth'))
+            print('ret', ret)
+            r_code = ret['code']
+            print('code', r_code)
+            if r_code != 0:
+                raise Exception('Fail when authenticate')
+            config['token'] = ret['token']
+
+        except Exception as e:
+            print(e)
+            sys.exit(0)
+
         # TASK
-        if action == 'getTasks':
+        if args.subcommand == 'getTasks':
             ret = json.loads(nut.sendData(packData(config, 'getTasks', {})))
             print(ret)
             for o in ret['dataout']:
                 print(o)
 
-        if action == 'getTask':  # task name
-            out = nut.sendData(packData(config, 'getTask', {'tname': args[0]}))
+        if args.subcommand == 'getTask':  # task name
+            out = nut.sendData(packData(config, 'getTask', {'tname': args.name}))
+            print(out)
             open('gr.zip', 'wb').write(base64.b64decode(out['dataout']['gradefolder']))
             out['dataout']['gradefolder'] = None
-            print(out)
 
-        if action == 'updateTask':  # inifile, zip file TODO test
+        if args.subcommand == 'updateTask':
             out = nut.sendData(packData(config, 'updateTask',
-                                        {'config': filetoSend(args[0]),
-                                         'gradefolderzipct': filetoSend(args[1])}))
+                                        {'config': filetoSend(args.taskconfig),
+                                         'gradefolderzipct': filetoSend(args.zipgrade)}))
             print(out)
 
         # REPORT
-        if action == 'reportByTaskName':  # name
-            ret = nut.sendData(packData(config, 'reportByTaskName', {'tname': args[0]}))
+        if args.subcommand == 'reportByTaskName':  # name
+            ret = nut.sendData(packData(config, 'reportByTaskName', {'tname': args.name}))
             print(ret)
             ret = json.loads(ret)
-            # f = open('t.zip', 'wb')
-            # f.write(base64.b64decode(ret['dataout']['content']))
-            # f.close()
-            # print('done')
-            action = 'getResultToken'
-            args[0] = ret['token']
+            args.subcommand = 'getResultToken'
+            args.token = ret['token']
 
-        if action == 'reportByTask':
-            zipfile = nut.sendData(packData(config, 'reportByTask', {'tid': args[0]}))  # id
-            print(zipfile)
-            f = open('out.zip', 'wb')
-            f.write(base64.b64decode(zipfile['dataout']['content']))
-            f.close()
-            print('done')
-
-        if action == 'reportByTaskLastest':  # id  (only get lastest)
-            zipfile = nut.sendData(packData(config, 'reportByTaskLastest', {'tid': args[0]}))
-            f = open('t.zip', 'wb')
-            f.write(base64.b64decode(zipfile['dataout']['content']))
-            f.close()
-            print('done')
-
-        # USER
-        # input file: many line, one line include user,group
-        if action == 'getGroups':
+        # USER, GROUP
+        if args.subcommand == 'getGroups':
             ret = json.loads(nut.sendData(packData(config, 'getGroups', {})))
             for o in ret['dataout']:
                 print(o)
 
-        if action == 'cleanGroup':  # name
-            ret = nut.sendData(packData(config, 'cleanGroup', {'gname': args[0]}))
+        if args.subcommand == 'cleanGroup':  # name
+            ret = nut.sendData(packData(config, 'cleanGroup', {'gname': args.name}))
             print(ret)
 
-        if action == 'addUsersGroup':  # file name: user,group in each line
+        if args.subcommand == 'addUsersGroup':  # file name: user,group in each line
             ret = nut.sendData(packData(config, 'addUsersGroup', {
-                'lst': [(line.split(',')[0].strip(), line.split(',')[1].strip()) for line in open(args[0], 'r')]}))
+                'lst': [(line.split(',')[0].strip(), line.split(',')[1].strip()) for line in open(args.file, 'r') if
+                        line.strip() != ""]}))
             print(ret)
-        if action == 'getUsersByGroup':  # groupName
-            out = nut.sendData(packData(config, 'getUsersByGroup', {'gname': args[0]}))
+        if args.subcommand == 'getUsersByGroup':  # groupName
+            out = nut.sendData(packData(config, 'getUsersByGroup', {'gname': args.name}))
             print(out)
 
         # PERMIT
-        if action == 'showAllPermit' or action == 'getPermits':
+        if args.subcommand == 'showAllPermit' or args.subcommand == 'getPermits':
             ret = json.loads(nut.sendData(packData(config, 'showAllPermit', {})))
             # print(ret)
             for o in ret['dataout']:
                 print(o)
 
-        if action == 'addPermit':  # add new or update permit; parameters: gname, tname, open, close
+        if args.subcommand == 'addPermit':  # add new or update permit; parameters: gname, tname, open, close
             out = nut.sendData(packData(config, 'addPermit',
                                         {'gname': args[0], 'tname': args[1], 'gopen': args[2], 'gclose': args[3]}))
             print(out)
 
-        if action == 'getResultToken':
+        if args.subcommand == 'getResultToken':
             is_done = False
             while not is_done:
-                out = nut.sendData(packData(config, 'getResultToken',
-                                            {'token': args[0]}))
+                out = nut.sendData(packData(config, 'getResultToken', {'token': args.token}))
                 out = json.loads(out)
                 # print(out)
                 pprint.pprint(out)
@@ -139,7 +162,6 @@ def main(args):
                 try:
                     f = open('t.zip', 'wb')
                     f.write(base64.b64decode(out['dataout']['content']))
-                    # f.write(out['dataout']['content'].encode('ascii'))
                     f.close()
                     print('done')
                     is_done = True
@@ -155,8 +177,9 @@ def packData(config, action, params):
     :param params:
     :return:
     """
-    return {'authenicate': {'username': config['username'], 'password': config['password']},
-            'data': {'action': action, 'params': params}}
+    return {  # 'authenicate': {'username': config['username'], 'password': config['password']},
+        'token': config['token'],
+        'data': {'action': action, 'params': params}}
 
 
 def encodeBin(binData):
@@ -197,9 +220,10 @@ class NetworkUtils:
         """"""
         # print(self.config)   
 
-    def sendData(self, load):
-        print(load)
-        url = host + '/onlinejudge/ws/lecturerws'
+    def sendData(self, load, url=None):
+        print('load', load)
+        if url is None:
+            url = host + '/onlinejudge/ws/lecturerws'
         headers = {'content-type': 'application/json; charset=utf-8'}
 
         r = requests.post(url, data=json.dumps(load), headers=headers, verify=False)
@@ -210,13 +234,5 @@ class NetworkUtils:
         #########################################################
 
 
-# entry point
-# sys.argv
-# argv 1 is name of action, argv 2, 3... is more details
-print(str(sys.argv))
-
 if __name__ == '__main__':
-    with open('lecturer_client.json', 'r') as f:
-        config = json.loads(f.read())
-
-    main(sys.argv[1:])
+    main()
